d50219df86501d620b34dbc014bebd9055c41161
Rizky Achmad Hafidhin <94460857+rizkyachmadhafidhin@users.noreply.github.com>
2022-07-17 12:33:02 +0700

    Add files via upload

---
+++ kurang.js
@@ -0,0 +1,6 @@
+function kurang() {
+	var angka1=parseInt(document.getElementById('a1').value);
+	var angka2=parseInt(document.getElementById('a2').value);
+	var hasil = angka1 - angka2;
+	document.getElementById('h').value = hasil;
+}
